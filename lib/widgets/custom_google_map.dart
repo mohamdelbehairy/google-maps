7d92c4a8f17a6a7045f9f60e34ac8f4896cf435f
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-08-29 14:14:39 +0200

    polylines part two

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_maps/models/place_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:ui' as ui;
 
 class CustomGoogleMap extends StatefulWidget {
   const CustomGoogleMap({super.key});
@@ -16,13 +13,13 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   @override
   void initState() {
     initialCameraPosition = const CameraPosition(
-        zoom: 12, target: LatLng(31.110648030353488, 30.93923980939196));
-    // initMarkers();
-    initPolyLines();
+        zoom: 10, target: LatLng(31.110648030353488, 30.93923980939196));
+    initPolygons();
     super.initState();
   }
 
   late GoogleMapController googleMapController;
+  Set<Polygon> polygons = {};
 
   @override
   void dispose() {
@@ -30,16 +27,12 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     super.dispose();
   }
 
-  Set<Marker> markers = {};
-  Set<Polyline> polylines = {};
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: GoogleMap(
             zoomControlsEnabled: false,
-            markers: markers,
-            polylines: polylines,
+            polygons: polygons,
             onMapCreated: (controller) {
               googleMapController = controller;
               initMapStyle();
@@ -54,40 +47,17 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     googleMapController.setMapStyle(nightMapStyle);
   }
 
-  Future<Uint8List> getImageFromRawData(String image, double width) async {
-    var imageData = await rootBundle.load(image);
-    var imageCodec = await ui.instantiateImageCodec(
-        imageData.buffer.asUint8List(),
-        targetWidth: width.round());
-    var imageFrame = await imageCodec.getNextFrame();
-    var imageByteData =
-        await imageFrame.image.toByteData(format: ui.ImageByteFormat.png);
+  void initPolygons() {
+    var polygon = Polygon(
+        fillColor: Colors.black.withOpacity(.5),
+        strokeWidth: 3,
+        polygonId: const PolygonId("1"),
+        points: const [
+          LatLng(31.178443621752827, 30.784911395646326),
+          LatLng(31.169097772274174, 31.043265987024547),
+          LatLng(30.971524047815137, 30.958844977186),
+        ]);
 
-    return imageByteData!.buffer.asUint8List();
-  }
-
-  void initMarkers() async {
-    var customMarkerItem = BitmapDescriptor.bytes(
-        await getImageFromRawData('assets/images/icons8-marker-50.png', 30));
-    var myMarker = places
-        .map((e) => Marker(
-            icon: customMarkerItem,
-            infoWindow: InfoWindow(title: e.name),
-            markerId: MarkerId(e.id.toString()),
-            position: e.latlng))
-        .toSet();
-
-    markers.addAll(myMarker);
-    setState(() {});
-  }
-
-  void initPolyLines() {
-    var polyline = const Polyline(polylineId: PolylineId("1"), points: [
-      LatLng(31.140924852458674, 30.93348721147604),
-      LatLng(31.090790537070333, 31.005576033302255),
-      LatLng(31.068467664448924, 30.93709552829787),
-      LatLng(31.0137693995647, 31.006040741882686)
-    ]);
-    polylines.add(polyline);
+    polygons.add(polygon);
   }
 }
